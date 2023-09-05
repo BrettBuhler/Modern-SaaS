8afb37840a8eeae0fe134ce1402be703730155b0
Brett Buhler <107363334+BrettBuhler@users.noreply.github.com>
2023-09-04 20:18:28 -0600

    3.4

---
+++ src/routes/logout/+server.ts
@@ -0,0 +1,12 @@
+import type { RequestHandler } from "@sveltejs/kit"
+import { error, redirect } from "@sveltejs/kit"
+
+export const POST: RequestHandler = async (event) => {
+    const { error: logoutError } = await event.locals.supabase.auth.signOut()
+
+    if (logoutError) {
+        throw error(500, "Error logging out out. Please try again.")
+    }
+
+    throw redirect(302, "/login")
+}
